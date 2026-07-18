a421547262fb6d4ba48814b883bbe48a1db572c1
sahilkr28 <piyushkr13579@gmail.com>
2026-07-18 11:51:56 +0530

    BST initial

---
+++ 02_DSA/BinarySearchTree/01.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root) {
+            if (root->val == val)
+                return root;
+            else if (val < root->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return nullptr;
+    }
+};
